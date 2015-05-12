443e08dd1399f9ce167329cdec17410edcbf5c0d
Rafael Moraes <contact@moraesrafael.com>
2015-05-11 20:48:27 -0400

    CORS; bugfixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,47 @@
 class ApplicationController < ActionController::Base
+  before_filter :document, only: :show
+  before_filter :cors, only: [ :show, :create ]
+
   def index
     render nothing: true
   end
 
   def show
-    document = Document.find_by_slug params[:slug]
-    return render nothing: true if document.nil? or document.url.blank?
+    return render nothing: true if @document.nil? or @document.url.blank?
     respond_to do |format|
-     format.html { redirect_to document.url_frag, status: :moved_permanently }
-     format.json { render json: { data: document.data } }
+     format.html { redirect_to @document.destination, status: :moved_permanently }
+     format.json { render json: { data: @document.data } }
     end
   end
 
   def create
-    document = Document.create(document_params)
-    render json: { slug: document.slug }
+    @document = Document.new
+    @document.url = params[:url]
+    @document.data = params[:data]
+    if @document.save
+      render json: { slug: @document.slug }
+    else
+      render nothing: true, status: :service_unavailable
+    end
   end
 
   protected
 
-  def document_params
-    params.permit(:url, :data)
+  def document
+    @document = Document.find_by_slug params[:slug]
   end
+
+  def cors
+    origin = request.headers['Origin']
+    unless origin.blank?
+      if @document
+        if origin.index(@document.origin)===0
+          headers['Access-Control-Allow-Origin'] = @document.origin
+        end
+      else
+        headers['Access-Control-Allow-Origin'] = '*'
+      end
+    end
+  end
+
 end
--- app/models/document.rb
+++ app/models/document.rb
@@ -2,11 +2,25 @@ class Document < ActiveRecord::Base
 
   after_create :slug_g
 
-  def url_frag
+  # def data=(data)
+  #   self[:data] = data.is_a?(String) ? data.to_json : data
+  # end
+
+  def destination
     return if self.url.blank?
     uri = URI.parse(self.url)
     uri.fragment = self.slug
     uri.to_s
+  end
+
+  def origin
+    return if self.url.blank?
+    uri = URI.parse(self.url)
+    origin = URI('')
+    origin.scheme = uri.scheme
+    origin.host = uri.host
+    origin.port = uri.port
+    origin.to_s
   end
 
   protected
